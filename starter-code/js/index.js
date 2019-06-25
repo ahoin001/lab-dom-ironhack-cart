3c1c8b61fbde7a09d951899212c81a5bfc5f6089
Alexander Hoinville <ahoin001@gmail.com>
2019-06-24 20:46:30 -0400

    Finished calculate (but want two decimals to show)

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,52 +1,82 @@
-function deleteItem(e){
+/*****************************
+        
+  Global DOM Elements 
+
+*****************************/
+
+// get a reference to the calculate button
+let button = document.getElementById("calculate-button");
+
+
+/*****************************
+        
+  Calculate Price  
+
+*****************************/
+
+//create event to occur when that button is clicked
+button.onclick = function () {
+
+  console.log("button clicked")
+
+  // points to the element containing price in DOM
+  let price = document.getElementById("price");
+
+  // points to the input to get the amount the user typed in later on
+  let quantity = document.querySelector("input");
+
+  // show myself the value inside both the above elements to make sure its what i expect
+  console.log(quantity.value);
+  console.log(Number(price.innerText));
+
+  //calculate total price and hold it in variable
+  let totalPrice = parseFloat(price.innerText).toFixed(2) * parseFloat(quantity.value).toFixed(2);
+  console.log(totalPrice);
+
+  // points to the element that shows the total shown to user 
+  let totalCostShownToUser = document.querySelector("#total");
+
+  console.log(totalCostShownToUser);
+
+  // display total cost to the user
+  totalCostShownToUser.innerHTML = String(totalPrice);
 
 }
 
-function getPriceByProduct(itemNode){
+
+function createQuantityInput() {
 
 }
 
-function updatePriceByProduct(productPrice, index){
+function createDeleteButton() {
 
 }
 
-function getTotalPrice() {
+function createQuantityNode() {
 
 }
 
-function createQuantityInput(){
+function createItemNode(dataType, itemData) {
 
 }
 
-function createDeleteButton(){
+function createNewItemRow(itemName, itemUnitPrice) {
 
 }
 
-function createQuantityNode(){
+function createNewItem() {
 
 }
 
-function createItemNode(dataType, itemData){
+// window.onload = function(){
+//   var calculatePriceButton = document.getElementById('calc-prices-button');
+//   var createItemButton = document.getElementById('new-item-create');
+//   var deleteButtons = document.getElementsByClassName('btn-delete');
 
-}
+//   // // calculatePriceButton.onclick = getTotalPrice;
+//   // createItemButton.onclick = createNewItem;
 
-function createNewItemRow(itemName, itemUnitPrice){
-
-}
-
-function createNewItem(){
-
-}
-
-window.onload = function(){
-  var calculatePriceButton = document.getElementById('calc-prices-button');
-  var createItemButton = document.getElementById('new-item-create');
-  var deleteButtons = document.getElementsByClassName('btn-delete');
-
-  calculatePriceButton.onclick = getTotalPrice;
-  createItemButton.onclick = createNewItem;
-
-  for(var i = 0; i<deleteButtons.length ; i++){
-    deleteButtons[i].onclick = deleteItem;
-  }
-};
+//   // for(var i = 0; i<deleteButtons.length ; i++){
+//   //   deleteButtons[i].onclick = deleteItem;
+//   // }
+// };
